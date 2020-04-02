66a584b1cc43dfd1c14e9de994eafc495b295cdf
Aleksandar Misljenovic <a.misljenovic@contractors.accruent.com>
2020-04-02 23:31:59 +0200

    added director column in movies table

--- Cinema.WebApi/Migrations/20200402213044_updateMoviesTable.Designer.cs
+++ Cinema.WebApi/Migrations/20200402213044_updateMoviesTable.Designer.cs
@@ -3,14 +3,16 @@ using Cinema.WebApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cinema.WebApi.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20200402213044_updateMoviesTable")]
+    partial class updateMoviesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
